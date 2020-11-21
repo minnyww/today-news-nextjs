e17adbd2f2a307eec3783448c38b38dd1fc73a83
minnyww <minrrverse@gmail.com>
2020-11-21 19:09:43 +0700

    [init]

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,108 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { useState } from "react";
+import useDebounce from "../utils/useDebounce";
+import useSWR from "swr";
+
+const KEY = "a656cda44d3b4195aa2e6e492de8c7cb";
+const fetcher = (url) => fetch(url).then((response) => response.json());
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+   //  const initialData = initail;
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+   const [inputValue, setInputValue] = useState(0);
+   const value = useDebounce(inputValue, 1000);
+   console.log("value : ", value);
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+   const { data } = useSWR(
+      `https://newsapi.org/v2/everything?q=${value || "man u"}&apiKey=${KEY}`,
+      // `http://newsapi.org/v2/top-headlines?country=${
+      //    value || "th"
+      // }&apiKey=${KEY}`,
+      fetcher,
+      { revalidateOnFocus: false },
+   );
+   if (data?.status === "error") return <div>error</div>;
+   console.log("data : ", data?.articles);
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+   return (
+      <div className="flex justify-center">
+         <Head>
+            <title>Min News</title>
+            <link rel="icon" href="/favicon.ico" />
+         </Head>
+         <div className="flex flex-col">
+            <div className="text-3xl px-4 mb-4 mt-4">
+               <h2>Today NEWS</h2>
+            </div>
+            <div className="px-4 mb-4 mt-4 mx-auto w-full">
+               <div className="relative mr-6 my-2 mx-auto w-full">
+                  <input
+                     onChange={(event) => setInputValue(event.target.value)}
+                     type="search"
+                     className="bg-purple-white shadow rounded border-0 p-3 w-full"
+                     placeholder="Search by news..."
+                  />
+                  <div className="absolute pin-r pin-t mt-3 mr-4 text-purple-lighter"></div>
+               </div>
+            </div>
+            {!data && "loading..."}
+            {data?.articles.length > 0 &&
+               data?.articles.map((article, index) => {
+                  return (
+                     <div
+                        className="mx-auto px-4 max-w-xl my-2"
+                        key={index}
+                        onClick={() => (window.location.href = article?.url)}
+                     >
+                        <div className="bg-white shadow-2xl rounded-lg mb-6 tracking-wide">
+                           <div className="md:flex-shrink-0">
+                              <img
+                                 // width="120"
+                                 // height="120"
+                                 src={article.urlToImage}
+                                 alt="mountains"
+                                 className="w-full h-64 rounded-lg rounded-b-none"
+                              />
+                           </div>
+                           <div className="px-4 py-2 mt-2">
+                              <h2 className="font-bold text-2xl text-gray-800 tracking-normal">
+                                 {article?.title}
+                              </h2>
+                              <p className="text-sm text-gray-700 px-2 mr-1">
+                                 {article?.description}
+                              </p>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+                              <div className="author flex items-center -ml-3 my-3">
+                                 <div className="user-logo">
+                                    <img
+                                       className="w-12 h-12 object-cover rounded-full mx-4  shadow"
+                                       src={article.urlToImage}
+                                       alt="avatar"
+                                    />
+                                 </div>
+                                 <h2 className="text-sm tracking-tighter text-gray-900">
+                                    <a href="#">By {article?.source?.name}</a>{" "}
+                                    <span className="text-gray-600">
+                                       {new Date(
+                                          article?.publishedAt,
+                                       ).toLocaleString()}
+                                    </span>
+                                 </h2>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  );
+               })}{" "}
+         </div>
+      </div>
+   );
 }
+
+// export async function getServerSideProps() {
+//    const data = await fetcher(
+//       `https://newsapi.org/v2/everything?q=${"premier league"}&apiKey=${KEY}`,
+//    );
+//    return { props: { data } };
+// }
